9b354c2f3f76d4a3b35db46299c9fcd6b1e5ada3
lupyuen <lupyuen@gmail.com>
2015-08-20 20:43:22 +0800

    Added remote access for SSH and web server

--- send_loop.sh
+++ send_loop.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+##  Change to the directory containing our scripts.
+cd /home/pi/GrovePi/Software/Python/
+
+##  Start the TCP tunnel for SSH.
+./ngrok tcp --log "stdout" -config=/home/pi/.ngrok2/ngrok.yml --remote-addr=???.tcp.ngrok.io:??? 22 &
+
+##  Start the Python web server.
+sudo python server.py &
+
+##  Wait a while for the web server to start up before starting the HTTP tunnel.
+sleep 10
+
+##  Start the HTTP tunnel for remote web access to our web server.
+./ngrok http --log "stdout" -config=/home/pi/.ngrok2/ngrok.yml --subdomain=??? 80 &
+
+##  Loop forever, sending sensor data.
 for (( ; ; ))
 do
 	##  Send sensor data.
